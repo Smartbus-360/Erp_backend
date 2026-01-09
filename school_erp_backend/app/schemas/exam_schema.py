bcf486c8e7325fa4c4671d11dd85e240894bf699
Smartbus-360 <smartbus365@gmail.com>
2026-01-09 16:08:59 +0530

    Update exam_schema.py

--- school_erp_backend/app/schemas/exam_schema.py
+++ school_erp_backend/app/schemas/exam_schema.py
@@ -3,10 +3,10 @@ from datetime import date
 
 class ExamCreate(BaseModel):
     name: str
-    class_id: int
+    # class_id: int
     start_date: date
     end_date: date
-    subject_ids: list[int]
+    # subject_ids: list[int]
 
 
 class ExamResponse(BaseModel):
