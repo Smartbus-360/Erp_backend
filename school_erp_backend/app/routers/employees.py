5bd601e0d40c9bf2315ea0708fe9adf007452d7d
Smartbus-360 <smartbus365@gmail.com>
2026-01-16 12:49:18 +0530

    Update employees.py

--- school_erp_backend/app/routers/employees.py
+++ school_erp_backend/app/routers/employees.py
@@ -25,7 +25,7 @@ def add_employee(
 ):
     employee = Employee(
     name=data.name,
-    designation=data.designation,
+    role_id=data.role_id,
     phone=data.phone,
     gender=data.gender,
     education=data.education,
